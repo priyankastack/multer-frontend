8ce792fbf0b4712b71c1654441303d842f76bf37
Priyanka singh <160533231+PRIYANKA22BSC@users.noreply.github.com>
2025-04-08 17:46:51 +0530

    ..

--- multer1/src/context/context.jsx
+++ multer1/src/context/context.jsx
@@ -1,11 +1,7 @@
 import  React from 'react';
 import { createContext,useState } from 'react';
 
-
-
  export const ImageContext=createContext();
-
-
 const ImageProvider=({children})=>{
 
  const [data, setData] = useState([]);
